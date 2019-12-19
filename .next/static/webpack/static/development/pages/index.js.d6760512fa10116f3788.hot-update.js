124696b9370759206b90391b04d627e9bfa41475
tetty <tettyneec0217@gmail.com>
2019-12-19 15:28:20 +0900

    initial commit

---
+++ .next/static/webpack/static/development/pages/index.js.d6760512fa10116f3788.hot-update.js
@@ -0,0 +1,56 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.tsx":
+/*!*************************!*\
+  !*** ./pages/index.tsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/regenerator */ "./node_modules/@babel/runtime-corejs2/regenerator/index.js");
+/* harmony import */ var _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+var _jsxFileName = "/Users/owner/Desktop/studies/javascript/next/next-ts/pages/index.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1__["createElement"];
+
+
+var Home = function Home() {
+  return __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Hello world!");
+};
+
+Home.getInitialProps = function _callee(_ref) {
+  var req, userAgent;
+  return _babel_runtime_corejs2_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          req = _ref.req;
+          userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
+          return _context.abrupt("return", {
+            userAgent: userAgent
+          });
+
+        case 3:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.d6760512fa10116f3788.hot-update.js.map
